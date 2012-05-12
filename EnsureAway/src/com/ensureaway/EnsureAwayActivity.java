d9b9c94d4fd6ce43c20af87520b5a9e702a39aa7
tslater2006 <tslater2006@gmail.com>
2012-05-11 19:56:54 -0500

    Initial Commit

---
+++ EnsureAway/src/com/ensureaway/EnsureAwayActivity.java
@@ -0,0 +1,13 @@
+package com.ensureaway;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnsureAwayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
