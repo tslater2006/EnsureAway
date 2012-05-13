bfd221ca3774425a3f789875fdc0be0f47a9034c
tslater2006 <tslater2006@gmail.com>
2012-05-13 08:01:24 -0500

    Testing Lock Device via a BroadcastReceiver and not a service

---
+++ EnsureAway/src/com/ensureaway/receivers/LockReceiver.java
@@ -0,0 +1,15 @@
+package com.ensureaway.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class LockReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
